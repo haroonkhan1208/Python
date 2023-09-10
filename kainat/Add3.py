084a1c3083a0416139d72dbfaf64b7856ca71942
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-10 06:44:44 +0000

    commit

---
+++ kainat/Add3.py
@@ -0,0 +1,5 @@
+fNum = int(input("Enter the first number :" ));
+sNum = int(input("Enter the second number :" ));
+tNum = int(input("Enter the third number  :" ));
+add = fNum + sNum + tNum ;
+print("Total is" ,add);
