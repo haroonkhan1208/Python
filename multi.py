ebe05dca8882c6f270fda3a5fde583562ce8cd8c
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-10 05:52:10 +0000

    Commit

---
+++ multi.py
@@ -0,0 +1,6 @@
+fnum = int(input("Enter the number : "));
+snum = int(input("Enter the number : "));
+
+mult = fnum * snum;
+
+print("The total is : ", mult);
---
+++ sub.py
@@ -0,0 +1,6 @@
+fnum = int(input("Enter the first number"));
+snum = int(input("Enter the second number"));
+
+sub = fnum - snum;
+
+print("Total", sub);
