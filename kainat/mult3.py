9ec95db754fb74e5faf88bdb2e627cdf816d4f4b
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-16 05:48:33 +0000

    commit

---
+++ kainat/mult3.py
@@ -0,0 +1,5 @@
+fNum = int(input("Enter the first number  : " ));
+sNum = int(input("Enter the second number  : " ));
+tNum = int(input("Enter the third number  : " ));
+mul = fNum * sNum * tNum ;
+print("the total is" , mul);
