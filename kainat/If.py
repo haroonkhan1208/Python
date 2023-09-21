271fc08e8b96e0be0af0320ee32eaa0a77ebf2ce
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-21 15:16:23 +0000

    commit

--- kainat/If.py
+++ kainat/If.py
@@ -1,5 +1,11 @@
-x = 6 ;
-if(x == 5):
+import random;
+
+x = int(input("Enter the number : "));
+y = random.randint(0,5);
+
+if(x == y):
     print("number is match");
 else:
-    print("number is not match");
+    print("number is not match and the correct number is : ", y);
+
+
