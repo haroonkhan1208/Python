a542d776f7ea96855fd9bf1fb307efb04372dd87
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-22 16:07:17 +0000

    commit

---
+++ Haroon/IdentifyTheNumber.py
@@ -0,0 +1,10 @@
+user = int(input("Enter the amount  : "));
+
+if(user < 20):
+    print("The number is smaller than 20");
+
+elif(user == 20):
+    print("The number is equal to 20");
+
+else:
+    print("The number is greater than 20");
