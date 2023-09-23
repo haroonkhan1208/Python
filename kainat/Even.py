8deb384e55c6a5ee37bbb4d3369fb30d8089c07c
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-23 07:02:38 +0000

    commit

--- kainat/Even.py
+++ kainat/Even.py
@@ -1,6 +1,6 @@
 a = int(input( "Enter the number  : " ));
-if =(a%2 == 0); 
+if(a%2 == 0):  
     print( a ," is even number ");
 else:
-    print("The odd number is ");
+    print(a , " is odd number ");
         
