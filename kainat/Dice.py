b77e56508bcfcda13fdadeee191fa23072a42b23
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-24 05:03:56 +0000

    commit

---
+++ kainat/Dice.py
@@ -0,0 +1,6 @@
+fdice = int(input("Enter the first dice  :  " ));
+sdice = int(input("Enter the second dice  :  " ));
+if(fdice <= 6 and sdice >= 6 ):
+    print("yes the dice is greater and equal than 6 ");
+add = (fdice + sdice );
+print("no the dice is not greater or equal than 10 ");
