6f1c78539b7ba7ce7a5c71bd3008892b15313aab
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-27 16:42:12 +0000

    commit

---
+++ Shivam/Dice.py
@@ -0,0 +1,13 @@
+fNum  = int(input("Enter the number  :  " ));
+sNum  = int(input("Enter the number  ;  " ));
+
+if( fNum <= 6 and sNum <= 6 ):
+    add = fNum + sNum ;
+    if(add <= 10):
+        print("YES");
+
+else:
+    print("No");
+    
+else:
+print("Invalid Input");
