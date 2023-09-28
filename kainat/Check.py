8f0c671d847f0bfb877032feca46fd719fc42c6b
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-28 10:00:47 +0000

    commit

---
+++ kainat/Check.py
@@ -0,0 +1,8 @@
+firstNumber = int(input("Enter the first Number : "));
+secondNumber = int(input("Enter the second number : "));
+
+if(firstNumber > secondNumber):
+    print("The difference : ", firstNumber - secondNumber);
+
+else:
+    print("Invalid Input");
