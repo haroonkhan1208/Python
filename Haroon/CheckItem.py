5045b4f1071b58311030c21935debd352244d1a5
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-28 10:48:44 +0000

    commit

---
+++ Haroon/CheckItem.py
@@ -0,0 +1,15 @@
+fNum = int(input("Enter the Number : "));
+
+sNum = 0 - 3;
+print("A")
+
+tNum = 3 - 6;
+print("B")
+
+foNum = 6 - 9;
+print("C")
+
+fiNum = 10;
+print("D")
+
+print("invaid input")
