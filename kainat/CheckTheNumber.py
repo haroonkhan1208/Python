06ee6f0b1d4130059a04f1089ef780660bdd1b94
haroonkhan1208 <143934069+haroonkhan1208@users.noreply.github.com>
2023-09-29 10:59:04 +0000

    commit

---
+++ kainat/CheckTheNumber.py
@@ -0,0 +1,12 @@
+num  =  int(input("Enter the number  :  " ));
+
+if(num >= 10):
+    print("The number is greater or equal 10 ");
+    if(num > 7):
+      print("YES");
+    else:
+      print("No");
+else:
+   print("Invalid Input");     
+
+
